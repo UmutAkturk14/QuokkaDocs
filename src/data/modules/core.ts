1901f5e1aff164c87eb0c84659f90cbabfaa459f
Umut Akturk <umut.akturk14@gmail.com>
2025-06-09 18:55:25 +0200

    Core module docs added

---
+++ src/data/modules/core.ts
@@ -0,0 +1,8 @@
+import type { DocModule } from "../../types/documentation";
+
+export const coreModule: DocModule = {
+  id: "core",
+  name: "Core",
+  description: "Core functionality and utilities",
+  functions: ["$", "data", "each", "filter", "map"],
+};
