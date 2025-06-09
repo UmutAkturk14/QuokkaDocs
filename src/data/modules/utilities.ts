fa0ced6c476de2363ed036d18a9821cdeb890f44
Umut Akturk <umut.akturk14@gmail.com>
2025-06-09 18:56:14 +0200

    Utilities module docs added

---
+++ src/data/modules/utilities.ts
@@ -0,0 +1,8 @@
+import type { DocModule } from "../../types/documentation";
+
+export const utilitiesModule: DocModule = {
+  id: "utilities",
+  name: "Utilities",
+  description: "A collection of utility functions for various tasks.",
+  functions: [],
+};
